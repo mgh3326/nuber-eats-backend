d290ad61c15a2680436e4555bf2eab4bc8c6a838
robin <mgh3326@naver.com>
2021-01-08 07:45:22 +0900

    4.6.An Alternative Error

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -11,18 +11,19 @@ export class UserService {
   ) {}
 
   async createAccount({
-    email,
-    password,
-    role,
-  }: CreateAccountInput): Promise<string | undefined> {
+                        email,
+                        password,
+                        role,
+                      }: CreateAccountInput): Promise<{ ok: boolean; error?: string }> {
     try {
       const exists = await this.users.findOne({ email });
       if (exists) {
-        return 'There is a user with that email already';
+        return { ok: false, error: 'There is a user with that email already' };
       }
       await this.users.save(this.users.create({ email, password, role }));
+      return { ok: true };
     } catch (e) {
-      return "Couldn't create account";
+      return { ok: false, error: "Couldn't create account" };
     }
   }
 }
