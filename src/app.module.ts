c62755c5ec7c7117429162e8170f0bbacce0d23e
robin <mgh3326@naver.com>
2021-01-25 22:20:34 +0900

    temp production sync

--- src/app.module.ts
+++ src/app.module.ts
@@ -51,7 +51,7 @@ import { ScheduleModule } from '@nestjs/schedule';
             password: process.env.DB_PASSWORD,
             database: process.env.DB_NAME,
           }),
-      synchronize: process.env.NODE_ENV !== 'prod',
+      synchronize: true,
       logging:
         process.env.NODE_ENV !== 'prod' && process.env.NODE_ENV !== 'test',
       entities: [
